5f79f45ba7344900025419e0c3b3b26422dcbe80
Bibin Das B <bibindasappu1@gmail.com>
2024-01-09 12:31:08 +0530

    aospa.dependencies

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/Realme-SM8250/hardware_oplus.git -b uvite hardware/oplus
+git clone https://github.com/AOSPA/android_hardware_qcom_audio.git -b topaz-865 vendor/qcom/opensource/audio-hal/primary-hal
+git clone https://github.com/AOSPA/android_hardware_qcom_display.git -b topaz-865 hardware/qcom/display
+git clone https://github.com/AOSPA/android_hardware_qcom_gps.git -b topaz-legacy-component hardware/qcom/gps
+git clone https://github.com/AOSPA/android_hardware_qcom_media.git -b topaz-865 hardware/qcom/media
+git clone --depth 1 https://gitlab.com/ThankYouMario/android_prebuilts_clang-standalone.git -b 17 prebuilts/clang-standalone
+git clone https://git.codelinaro.org/clo/la/platform/hardware/nxp/nfc.git -b refs/tags/LA.QSSI.13.0.r1-09400-qssi.0 hardware/nxp/nfc
+git clone https://git.codelinaro.org/clo/la/platform/hardware/nxp/secure_element.git -b refs/tags/LA.QSSI.13.0.r1-09400-qssi.0 hardware/nxp/secure_element
+
